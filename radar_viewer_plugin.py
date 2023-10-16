c6cca599cb64ec9490adba6f827a3d47a7476ac0
Laura Lindzey <lindzey@uw.edu>
2023-10-15 23:44:46 -0700

    Generate spatial index from the QIceRadar index group's features

--- radar_viewer_plugin.py
+++ radar_viewer_plugin.py
@@ -7,7 +7,13 @@ import PyQt5.QtGui as QtGui
 import PyQt5.QtWidgets as QtWidgets
 
 import qgis.core
-from qgis.core import QgsMessageLog, QgsProject
+from qgis.core import (
+    QgsLayerTreeGroup,
+    QgsLayerTreeLayer,
+    QgsMessageLog,
+    QgsProject,
+    QgsSpatialIndex,
+)
 
 from .radar_viewer_widgets import (
     RadarViewerConfigurationWidget,
@@ -24,7 +30,12 @@ class RadarViewerPlugin(QtCore.QObject):
         super(RadarViewerPlugin, self).__init__()
         self.iface = iface
 
-        # Default value
+        # The spatial index needs to be created for each new project
+        # TODO: Consider whether to support user switching projects and
+        #  thus needing to regenerate the spatial index. (e.g. Arctic / Antarctic switch?)
+        self.spatial_index = None
+
+        # Try loading config when plugin initialized (before project has been selected)
         self.config = UserConfig()
         try:
             # Save to global QGIS settings, not per-project.
@@ -89,26 +100,93 @@ class RadarViewerPlugin(QtCore.QObject):
         )
         qs = QtCore.QSettings()
         qs.setValue("radar_viewer_config", yaml.safe_dump(config_dict))
+        # This is how to do it per-project, rather than globally
         # QgsProject.instance().writeEntry(
         #     "radar_viewer", "user_config", yaml.safe_dump(config_dict)
         # )
 
+    def build_spatial_index(self) -> None:
+        """
+        This is slow on my MacBook Pro, but not impossibly so.
+        """
+        QgsMessageLog.logMessage("Trying to build spatial index")
+        root = QgsProject.instance().layerTreeRoot()
+        qiceradar_group = root.findGroup("ANTARCTIC QIceRadar Index")
+        if qiceradar_group is None:
+            qiceradar_group = root.findGroup("ARCTIC QIceRadar Index")
+        if qiceradar_group is None:
+            errmsg = (
+                "Could not find index data. \n\n"
+                "You may need to drag the QIceRadar .qlr file into QGIS. \n\n"
+                "Or, if you renamed the index layer, please revert the name to either "
+                "'ANTARCTIC QIceRadarIndex' or 'ARCTIC QIceRadar Index' \n\n"
+                "Making the group selectable is an existing TODO."
+            )
+            message_box = QtWidgets.QMessageBox()
+            message_box.setText(errmsg)
+            message_box.exec()
+            return
+        else:
+            QgsMessageLog.logMessage(f"Found QIceRadar group!")
+
+        self.spatial_index = QgsSpatialIndex()
+        for institution_group in qiceradar_group.children():
+            if not isinstance(institution_group, QgsLayerTreeGroup):
+                # Really, there shouldn't be any, but who knows what layers the user may have added.
+                QgsMessageLog.logMessage(
+                    f"Unepected layer in QIceRadarIndex: {institution_group}"
+                )
+                continue
+            for campaign in institution_group.children():
+                if not isinstance(campaign, QgsLayerTreeLayer):
+                    QgsMessageLog.logMessage(
+                        f"Unexpected group in QIceRadarIndex{campaign}"
+                    )
+                    continue
+                # Quick sanity check that this is a layer for the index
+                try:
+                    feat = next(campaign.layer().getFeatures())
+                    # TODO: Would be better to have a proper function for checking this.
+                    for field in [
+                        "availability",
+                        "campaign",
+                        "institution",
+                        "granule",
+                        "segment",
+                        "region",
+                    ]:
+                        if field not in feat.attributeMap():
+                            QgsMessageLog.logMessage(
+                                f"Layer in {campaign} missing expected field {field}; not adding features to index."
+                            )
+                            break
+                    QgsMessageLog.logMessage(f"Adding features from {campaign.name()}")
+                    self.spatial_index.addFeatures(campaign.layer().getFeatures())
+                except Exception as ex:
+                    QgsMessageLog(f"{ex.what()}")
+
     def run(self) -> None:
         print("run")
+        # The QIceRadar tool is a series of widgets, kicked off by clicking on the icon.
 
-        # This kicks off a series of widgets
-        # Try to load overall configuration. This will include:
-        # * data directory
-        # * NSIDC credentials (optional; can be added later)
-        # * AAD credentials (likewise optional)
-        # if configuration not loaded
-        if self.config is not None and not config_is_valid(self.config):
+        # First, make sure we at least have the root data directory configured
+        if not config_is_valid(self.config):
             cw = RadarViewerConfigurationWidget(
                 self.iface, self.config, self.set_config
             )
+            # Config is set via callback, rather than direct return value
             cw.run()
 
-        QgsMessageLog.logMessage(f"Config = {self.config}; ready for use!")
+        if not config_is_valid(self.config):
+            QgsMessageLog.logMessage("Invalid configuration; can't start QIceRadar")
+            return
+        else:
+            QgsMessageLog.logMessage(f"Config = {self.config}; ready for use!")
+
+        # Next, try to create the spatial index
+        # TODO: Actually regenerate this whenever project is changed.
+        if self.spatial_index is None:
+            self.build_spatial_index()
 
         # QUESTION: What to do here while waiting for a mouse click?
         # On mouse click,
