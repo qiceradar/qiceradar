853738bff8fcc3a9ea8828c8a39174b7ba24d170
Laura Lindzey <lindzey@uw.edu>
2024-08-07 00:11:09 -0700

    Updates to display ALL Antarctic CReSIS surveys

--- datautils/cresis_utils.py
+++ datautils/cresis_utils.py
@@ -7,6 +7,19 @@ import scipy.io
 
 
 def load_radargram(filepath: pathlib.Path) -> Tuple[Any, Any, Any, Any, Any]:
+    """
+    The available fields change with season, though the core
+    variables have been consistent since 2002_Antarctica_P3.
+
+    The biggest difference is whether the .mat file  can
+    be opened with h5py, or if we have to use scipy.io
+    (This is a function of which version of Matlab was used to create it.)
+
+    Additionally, a single season reported complex radargrams; for
+    that one, we display the magnitude.
+    """"
+
+    print(f"load_radargram({filepath})")
     try:
         data = h5py.File(filepath, 'r')
         return extract_radargram_h5py(data)
@@ -23,23 +36,27 @@ def extract_radargram_h5py(data):
     radargram = data['Data']
     radargram = np.log(np.array(radargram))
 
-    lat = np.array(data['Latitude']).flatten()
-    lon = np.array(data['Longitude']).flatten()
+    lat = data['Latitude'][:].flatten()
+    lon = data['Longitude'][:].flatten()
 
-    utc = np.array(data['GPS_time']).flatten()
-    fast_time_us = data['Time'][0] # np.ndarray
-    # It also has Elevation, Roll, Pitch, Heading, Surface
+    utc = data['GPS_time'][:].flatten()
+    fast_time_us = 1e6 * data['Time'][:].flatten()
     return radargram, lat, lon, utc, fast_time_us
 
 def extract_radargram_scipy(data):
+    """
+    Older data needs to be imported using scipy.io.
+    """
     radargram = data['Data']
+    # 2005_GPRWAIS's data is reported as a complex array
+    if np.iscomplexobj(radargram):
+        radargram = np.abs(radargram)
     radargram = np.log(np.array(radargram).transpose())
 
-    lat = data['Latitude'][0]
-    lon = data['Longitude'][0]
+    lat = data['Latitude'].flatten()
+    lon = data['Longitude'].flatten()
 
-    utc = data['GPS_time'][0]
-    fast_time_us = data['Time'][0] # np.ndarray
+    utc = data['GPS_time'].flatten()
+    fast_time_us = 1e6 * data['Time'].flatten()
 
-    # It also has Elevation, Roll, Pitch, Heading, Surface
     return radargram, lat, lon, utc, fast_time_us
