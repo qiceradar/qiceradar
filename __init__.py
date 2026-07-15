4a45b5d6cdabeea7f3de6a6f2c34af9219884425
Laura Lindzey <lindzey@uw.edu>
2026-07-14 23:56:35 -0700

    convert h5py->pyfive in cresis_utils for easier install

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
+import pathlib
+import sys
+
+# We bundle some of our dependencies to get around the mess that it the (lack) # of coherent dependency management across QGIS plugins. The goal is to not
+# require users on any OS to pip-install anything.
+# For now, this includes:
+# * pyfive (pure python replacement to h5py)
+external_dir = pathlib.Path(__file__).resolve().parent / "external"
+sys.path.insert(0, str(external_dir / "pyfive"))
+
+
 def classFactory(iface):
     from .qiceradar_plugin import QIceRadarPlugin
 
