5815fa8ff71b0bf1fbf86a8d928a14c5a9fad96a
Laura Lindzey <lindzey@uw.edu>
2026-08-12 11:23:08 +0200

    Document steps to install vendored dependencies.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 import pathlib
 import sys
 
-# We bundle some of our dependencies to get around the mess that it the (lack) # of coherent dependency management across QGIS plugins. The goal is to not
+# We bundle some of our dependencies to get around the mess that is the (lack)
+# of coherent dependency management across QGIS plugins. The goal is to not
 # require users on any OS to pip-install anything.
-# For now, this includes:
-# * pyfive (pure python replacement to h5py)
+# TODO: Rather than changing sys.path everywhere, temporarily add it only
+#    for the import statements.
 external_dir = pathlib.Path(__file__).resolve().parent / "external"
 if external_dir not in sys.path:
     sys.path.insert(0, str(external_dir))
